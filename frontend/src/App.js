9812f5f01f77b941073b23fde7771479c49a3403
Vradash <singhvikramrajan111@gmail.com>
2024-06-09 12:47:39 +0530

    chore: Remove unused code and update routing in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import { useState } from 'react';
-// import axios from 'axios';
 
 import Navbar from './Components/navbar.js';
 import Footer from "./Components/footer.js";
@@ -11,25 +10,16 @@ import About from './Pages/about.js'
 import Shop from './Pages/shop.js'
 import Cart from './Pages/cart.js'
 import Product from './Pages/product.js';
-
 import './App.css';
-// import home from './Pages/home.js';
-
-
 
 function App() {
   const [user,setUser]= useState();
 
-  // const [userCart,setuserCart]= useState();
   const api=process.env.REACT_APP_API_URL;
-  // console.log(api);
 
   const userData=(data)=>{
     setUser(data?.data);
-    // console.log("from Parent",data?.data);
   }
-
-  // console.log(user?.data);
   
   if(!user){
     return <Login func={userData} apiURL={api}/>
@@ -43,13 +33,11 @@ function App() {
           else the navlink in navbar doesnt work*/}
           <Routes>
             <Route index element={<Home apiURL={api}/>}/>
-            {/* <Route path='/home' element={<Home />}/> */}
             <Route path='/about' element={<About />}/>
             <Route path='/contact' element={<Contact />}/>
             <Route path='/shop' element={<Shop apiURL={api}/>}/>
             <Route path='/cart' element={<Cart id={user._id} apiURL={api}/>}></Route>
             <Route path='/product' element={<Product id={user._id} apiURL={api}/>}></Route>
-            {/* <Route path='/navlink' element={<Navbar />}></Route> */}
           </Routes>
         </BrowserRouter>
 
@@ -61,37 +49,3 @@ function App() {
 }
 
 export default App;
-
-
-
-// const [input, setInput] = useState("");
-
-  // const url = 'https://real-time-product-search.p.rapidapi.com/search?q=smartphone&country=us&language=en';
-  // const options = {
-  //     method: 'GET',
-  //     headers: {
-  //         'X-RapidAPI-Key': '726de44217mshbf1db20a5c4eac7p183083jsn4ba17c63221f',
-  //         'X-RapidAPI-Host': 'real-time-product-search.p.rapidapi.com'
-  //     }
-  // };
-
-  // // fetch(url,options).then(res => res.json()).then(data => console.log(data));
-
-  // fetch('https://dummyjson.com/products/search?q=Laptop')
-  // .then(res => res.json())
-  // .then(json => console.log(json))
-
-  // useEffect(() => {
-  //   axios.get("https://dummyjson.com/products/search?q=Laptop")
-  //   .then(res => console.log(res));
-  // })
-
-  // const [data,setData] = useState([]);
-
-  // useEffect(() => {
-  //   axios.get("/api/product")
-  //   .then(res => setData(res.data))
-  //   .catch(err => console.log(err));
-  // },[]);
-
-  // console.log("Data is" , data);
